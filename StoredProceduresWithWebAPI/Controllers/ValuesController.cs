21eca4b94892864bf2568cce3bfc2f2fd50aee42
LONGMANKE <mburunjoroge0@gmail.com>
2024-04-24 07:03:15 +0300

    Add project files.

---
+++ StoredProceduresWithWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using StoredProceduresWithWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace StoredProceduresWithWebAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["webapi_conn"].ConnectionString);
+        Employee emp = new Employee();
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)  
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
